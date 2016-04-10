83c68f95d39dacf44d663cbec7cf5d821698810a
Alex Gofman <gofmanalex13@gmail.com>
2016-04-10 21:57:41 +0400

    tweaked main component

--- index.ios.js
+++ index.ios.js
@@ -1,34 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
+'use strict';
 
-import React, {
+const React = require('react-native');
+
+const Dimensions = require('Dimensions');
+
+const {
+  height,
+  weight
+} = Dimensions.get('window');
+
+const {
   AppRegistry,
-  Component,
   StyleSheet,
+  Component,
   Text,
-  View
-} from 'react-native';
-
-class ReactNativeTut extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+  View,
+  Navigator,
+  TouchableHighlight
+} = React;
 
 const styles = StyleSheet.create({
   container: {
@@ -41,12 +30,50 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    color: 'white'
   },
 });
 
-AppRegistry.registerComponent('ReactNativeTut', () => ReactNativeTut);
+const BaseConfig = Navigator.SceneConfigs.FloatFromRight;
+
+const CustomLeftToRightGesture = Object.assign({}, BaseConfig.gestures.pop, {
+  // Make it snap back really quickly after canceling pop
+  snapVelocity: 8,
+  // Make it so we can drag anywhere on the screen
+  edgeHitWidth: width
+});
+
+const CustomSceneConfig = Object.assign({}, BaseConfig, {
+  // A tightly wound spring will make this transition fast
+  springTension: 100,
+  springFriction: 1,
+  // Use our custom gesture defined above
+  gestures: {
+    pop: CustomLeftToRightGesture
+  }
+});
+
+class ReactNativeTutorial extends Component {
+
+  // A simple switch statement to allow our router to work
+  _renderScene(route, navigator) {
+
+  }
+
+  // Plug in the animations
+  _configureScene(route) {
+    return CustomSceneConfig;
+  }
+
+  render() {
+    return (
+      <Navigator
+        initialRoute={{id: 1}}
+        renderScene={this._renderScene}
+        configureScene={this._configureScene}
+			/>
+    )
+  }
+}
+
+AppRegistry.registerComponent('ReactNativeTutorial', () => ReactNativeTutorial);
